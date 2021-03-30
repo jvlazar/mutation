4f0dae694953282c46f11c5d02445e00e6170e58
jvlazar <56981054+jvlazar@users.noreply.github.com>
2021-03-29 23:10:30 -0600

    Update and rename delete508.py to delete.py

--- delete.py
+++ delete.py
@@ -12,24 +12,25 @@ target = target.upper() # converts input to uppercase
 position = int(input("What position do you want to delete?: "))
 
 count = 1
-char = fName.read(1)
-
-while True:
-    if char == ']':
-        char = fName.read(1)
-        break
-    else:
-        char = fName.read(1)
-
 
 while True: # loop indefinitely
-    char = fName.read(1) # read the char 1 byte at a time
+    char = fName.read(1)
+
     if not char: # if the value of char is null, exit out of the loop
         break
+
+    if char == ">": # if the file is in FASTA format read until you reach "]"
+        while True:
+            if char == "]":
+                char = fName.read(1)
+                break
+            else:
+                char = fName.read(1)
 
     if count != position: # when not at the desired position, add 1 to count
         if char != "\n":
             count = count + 1
+            print("The position is " + str(count) + " " + char)
         elif char == "\n": # if the character is a newline, don't change the count
             pass
 
@@ -39,6 +40,7 @@ while True: # loop indefinitely
     elif count == position and char == target:
         print("The amino acid at " + str(position) + " is " + char) # letting the user know that the aa is there
         count = count + 1 # increasing count
+
 
 
 fName.close()
